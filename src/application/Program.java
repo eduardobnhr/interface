370059ef2d531818dcbc864d494b2f0b6df62441
eduardo <eduardobnhr@gmail.com>
2023-07-13 08:59:31 -0400

    add

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,38 @@
 package application;
 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm");
+		
+		System.out.println("Entre com os dados do aluguel");
+		System.out.print("modelo do carro: ");
+		String carModel = sc.nextLine();
+		System.out.println("Retirada (dd/MM/yyyy HH:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		System.out.println("Retorno (dd/MM/yyyy HH:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		
+		sc.close();
 	}
 
 }
--- src/model/entities/Invoice.java
+++ src/model/entities/Invoice.java
@@ -1,5 +1,37 @@
 package model.entities;
 
 public class Invoice {
+	
+	private Double basicPaymente;
+	private Double tax;
+	
+	public Invoice() {
+		
+	}
+
+	public Invoice(Double basicPaymente, Double tax) {
+		this.basicPaymente = basicPaymente;
+		this.tax = tax;
+	}
+
+	public Double getBasicPaymente() {
+		return basicPaymente;
+	}
+
+	public void setBasicPaymente(Double basicPaymente) {
+		this.basicPaymente = basicPaymente;
+	}
+
+	public Double getTax() {
+		return tax;
+	}
+
+	public void setTax(Double tax) {
+		this.tax = tax;
+	}
+	
+	public Double getTotalPayment() {
+		return getBasicPaymente()- getTax();
+	}
 
 }
